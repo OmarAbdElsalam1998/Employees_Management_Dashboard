99c3b80bd5eff4120a577c8af52f64af58dfbbf5
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2023-08-16 23:34:18 +0300

    create rest of  pages

---
+++ assets/js/popUpConfirmationMessage.js
@@ -0,0 +1,24 @@
+// Start pop up
+const popUpBtns=document.querySelectorAll(".pop-up-btn");
+const popUpContainer=document.querySelector(".pop-up");
+const popUpInner=document.querySelector(".pop-up .inner");
+const closePopUpButton=document.querySelector(".pop-up .inner #ClosePopUP");
+popUpBtns.forEach(btn=>{
+    btn.addEventListener("click",function(){
+        popUpContainer.classList.add("show");
+      })
+})
+popUpContainer.addEventListener("click",function(){
+  this.classList.remove("show");
+})
+popUpInner.addEventListener("click",function(e){
+   e.stopPropagation();
+   console.log("ddd")
+})
+
+
+closePopUpButton.addEventListener("click",function(e){
+    e.preventDefault()
+    popUpContainer.classList.remove("show");
+
+ })
