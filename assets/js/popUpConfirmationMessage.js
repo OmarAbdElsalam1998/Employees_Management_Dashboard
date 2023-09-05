30e64ead69c2d11a5bf13488d2064b5f3f000ba4
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2023-09-06 00:37:00 +0300

    edit

--- assets/js/popUpConfirmationMessage.js
+++ assets/js/popUpConfirmationMessage.js
@@ -1,26 +1,26 @@
 // Start pop up
-const popUpBtns=document.querySelectorAll(".pop-up-btn");
-const popUpContainer=document.querySelector(".pop-up");
-const popUpInner=document.querySelector(".pop-up .inner");
-const closePopUpButton=document.querySelector(".pop-up .inner #ClosePopUP");
-popUpBtns.forEach(btn=>{
+  const popUpBtns=document.querySelectorAll(".pop-up-btn");
+  const popUpContainer=document.querySelector(".pop-up");
+  const popUpInner=document.querySelector(".pop-up .inner");
+  const closePopUpButton=document.querySelector(".pop-up .inner #ClosePopUP");
+  popUpBtns.forEach(btn=>{
 
-    btn.addEventListener("click",function(){
-        popUpContainer.classList.add("show");
-        console.log("gg")
-      })
-})
-popUpContainer.addEventListener("click",function(){
-  this.classList.remove("show");
-})
-popUpInner.addEventListener("click",function(e){
-   e.stopPropagation();
-   console.log("ddd")
-})
+      btn.addEventListener("click",function(){
+          popUpContainer.classList.add("show");
+          console.log("gg")
+        })
+  })
+  popUpContainer.addEventListener("click",function(){
+    this.classList.remove("show");
+  })
+  popUpInner.addEventListener("click",function(e){
+    e.stopPropagation();
+    console.log("ddd")
+  })
 
 
-closePopUpButton.addEventListener("click",function(e){
-    e.preventDefault()
-    popUpContainer.classList.remove("show");
+  closePopUpButton.addEventListener("click",function(e){
+      e.preventDefault()
+      popUpContainer.classList.remove("show");
 
- })
+  })
